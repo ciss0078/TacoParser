96313c25cc3aa8f12e6f69430bdb7173ae1ea7e6
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-11-28 15:07:11 -0800

    made correction to taco parser

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -39,30 +39,39 @@ namespace LoggingKata
             ITrackable name1 = null;
             ITrackable name2 = null;
             double dist = 0;
-            foreach(var locA in locations)
+            double currentdist = 0;
+            foreach (var locA in locations)
             {
+                
                 GeoCoordinate CorA = new GeoCoordinate();
                 CorA.Latitude = locA.Location.Latitude;
                 CorA.Longitude = locA.Location.Longitude;
                 
 
+
+                
                 foreach (var locB in locations)
-                {
-                    GeoCoordinate CorB = new GeoCoordinate();
+                {   GeoCoordinate CorB = new GeoCoordinate();
                     CorB.Latitude = locB.Location.Latitude;
                     CorB.Longitude = locB.Location.Longitude;
 
-                   var currentdist = CorA.GetDistanceTo(CorB);
+                   dist = CorA.GetDistanceTo(CorB);
 
-                    if(currentdist > dist)
+                    if (dist > currentdist)
                     {
                         name1 = locA;
                         name2 = locB;
                     }
-                    
-                }
 
-                
+
+
+
+
+                }
+                currentdist = dist;
+
+
+
             }
             Console.WriteLine($"{name1.Name}, {name1.Location.Latitude}, {name1.Location.Longitude}");
             Console.WriteLine($"{name2.Name}, {name2.Location.Latitude}, {name2.Location.Longitude}");
